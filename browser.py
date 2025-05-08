7365c2d6743106100309521b9b19b71388266cdb
Jacob Shogren <shogrenjacob@gmail.com>
2025-05-08 00:14:25 -0400

    Feature: window resizing

--- browser.py
+++ browser.py
@@ -2,63 +2,99 @@ import tkinter
 from displayHtml import lex, showHTML
 from url import URL
 
-WIDTH, HEIGHT = 800, 600
-HSTEP, VSTEP = 13, 18
-SCROLL_STEP = 100
-
 class Browser:
     def __init__(self):
+        self.WIDTH, self.HEIGHT = 800, 600
+        self.HSTEP, self.VSTEP = 13, 18
+        self.SCROLL_STEP = 100
+
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas = tkinter.Canvas(self.window, width=self.WIDTH, height=self.HEIGHT)
         self.canvas.pack()
-        self.scroll = 0
+        self.scroll = 1
         self.display_list = []
+        self.text = ""
+
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<MouseWheel>", self.scrollmouse)
+        self.window.bind("<Configure>", self.resize)
 
     def load(self, url: URL):
 
+        self.url = url
         body = url.request()
 
-        if url.subscheme == "view-source":
-            print("in load, url scheme: " + url.scheme)
-            text = showHTML(body)
-        else:
-            text = lex(body)
 
-        self.display_list = self.layout(text)
+        if url.subscheme == "view-source":
+            self.text = showHTML(body)
+        else:
+            self.text = lex(body)
+
+        self.display_list = self.layout(self.text)
         self.draw()
     
     def draw(self):
         for x, y, c in self.display_list:
 
             # Only create text that needs to be displayed, gets us closer to frame budget
-            if y > self.scroll + HEIGHT:
+            if y > self.scroll + self.HEIGHT:
                 continue
-            if y + VSTEP < self.scroll:
+            if y + self.VSTEP < self.scroll:
                 continue
             self.canvas.create_text(x, y - self.scroll, text=c)
 
+    # e denotes the keyup event
     def scrolldown(self, e):
         self.canvas.delete("all")
-        self.scroll += SCROLL_STEP
+        self.scroll += self.SCROLL_STEP
         self.draw()
 
     def scrollup(self, e):
         self.canvas.delete("all")
-        self.scroll -= SCROLL_STEP
-        self.draw()    
+        if self.scroll != 0:
+            self.scroll -= self.SCROLL_STEP
+        self.draw()
+
+    def scrollmouse(self, e):
+        if self.scroll > 0:
+            self.canvas.delete("all")
+
+            self.scroll -= e.delta * (self.SCROLL_STEP / 2)
+
+            if self.scroll <= 0:
+                self.scroll = 1
+
+            self.draw()
+        elif self.scroll <= 0:
+            self.scroll = 1
+        
+    def resize(self, e):
+        self.WIDTH = e.width
+        self.HEIGHT = e.height
+        self.canvas.pack(fill='both', expand=1)
+        
+        self.canvas.delete("all")
+        self.layout(self.text)
+        self.draw()
+
 
     def layout(self, text):
         self.display_list = []
-        cursor_x, cursor_y = HSTEP, VSTEP
+        cursor_x, cursor_y = self.HSTEP, self.VSTEP
 
         for c in text:
+            if c == '\n':
+                cursor_y += self.VSTEP + 8
+                cursor_x = self.HSTEP
+                continue
+
             self.display_list.append((cursor_x, cursor_y, c))
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+            
+            if cursor_x >= self.WIDTH - self.HSTEP:
+                cursor_y += self.VSTEP
+                cursor_x = self.HSTEP
             else:    
-                cursor_x += HSTEP
+                cursor_x += self.HSTEP
         return self.display_list
 
